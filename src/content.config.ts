0e987524b5edc322a6ee6ba98a1396b32854c009
Joacco <joaquingodoy2407@gmail.com>
2026-07-06 20:02:47 -0300

    style: order content config imports

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
 	loader: glob({ pattern: '*.json', base: './src/content/projects/en' }),
